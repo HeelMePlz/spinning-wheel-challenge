dd70ccd317ad56cae3be11ccd57dbdae17de3738
HeelMePlz <78705543+HeelMePlz@users.noreply.github.com>
2023-06-15 21:34:54 +0100

    function to get challenges from channel

--- bot.py
+++ bot.py
@@ -19,5 +19,37 @@ async def on_ready():
     print(f"{bot.user} has connected to Discord!")
     await bot.tree.sync()
 
+async def get_challenges():
+    challenges = []
+
+    channel_id = str(SPINNING_WHEEL_CHANNEL)
+    channel = bot.get_channel(int(channel_id))
+
+    async for message in channel.history():
+        if len(message.content) < 125:
+            text = message.content
+        else:
+            text = message.content[:125] + "..."
+
+        stripped_text = text.replace("\n", "")
+
+        reaction = discord.utils.get(message.reactions, emoji="⬆️")
+        reaction_count = reaction.count
+        user_id = message.author.id
+        username = message.author.name
+        message_url = message.jump_url
+
+        challenges.append(
+            {
+                "challenge": stripped_text,
+                "reactions": reaction_count,
+                "user": user_id,
+                "username": username,
+                "link": message_url,
+            }
+        )
+
+    return challenges
+
 
 bot.run(TOKEN)
